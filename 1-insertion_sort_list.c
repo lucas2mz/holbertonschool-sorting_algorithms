f361ea37eb4ddb98e548e7b8d5abb37b7ba703c1
lucas2mz <lucasandrada2606@gmail.com>
2024-12-15 00:10:26 +0000

    Task 1 (not finished)

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	int temp = 0, i, j;
+	listint_t *aux, *current;
+	
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		aux = current;
+		while ()
+		{
+		
+		}
+	}
+}
