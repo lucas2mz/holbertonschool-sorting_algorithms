3f78d1cae3d70ed3ebc65009f560ee44d32d656a
lucas2mz <lucasandrada2606@gmail.com>
2024-12-15 20:51:07 +0000

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,20 +3,44 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux, *current, *temp;
-	
+
+	if (list == NULL)
+		return;
+
 	current = (*list)->next;
 
 	while (current != NULL)
 	{
 		aux = current->prev;
+		temp = current->next;
+
 		while (aux != NULL && aux->n > current->n)
 		{
 			temp = current->next;
 
 			if (current->next != NULL)
 			{
-				
+				current->next->prev = current->prev;
 			}
+			if (current->prev != NULL)
+			{
+				current->prev->next = current->next;
+			}
+			current->next = aux;
+			current->prev = aux->prev;
+
+			if (aux->prev != NULL)
+			{
+				aux->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			aux->prev = current;
+			aux = current->prev;
+			print_list(*list);
 		}
+		current = temp;
 	}
 }
