025a09a3622ce6dc0d2c297217a055e4bc1d2563
lucas2mz <lucasandrada2606@gmail.com>
2024-12-15 20:56:35 +0000

    Betty Fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,8 @@
 #include "sort.h"
-
+/**
+ * insertion_sort_list - Sort a list with insertion sort
+ * @list: The list to sort
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux, *current, *temp;
@@ -13,30 +16,26 @@ void insertion_sort_list(listint_t **list)
 	{
 		aux = current->prev;
 		temp = current->next;
-
 		while (aux != NULL && aux->n > current->n)
 		{
 			temp = current->next;
 
 			if (current->next != NULL)
-			{
 				current->next->prev = current->prev;
-			}
+
 			if (current->prev != NULL)
-			{
 				current->prev->next = current->next;
-			}
+
 			current->next = aux;
 			current->prev = aux->prev;
 
 			if (aux->prev != NULL)
-			{
 				aux->prev->next = current;
-			}
 			else
 			{
 				*list = current;
 			}
+
 			aux->prev = current;
 			aux = current->prev;
 			print_list(*list);
